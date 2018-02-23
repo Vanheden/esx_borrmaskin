d488331d862d007e01fc23a8689631b1c3ce8846
Vanheden <33155577+Vanheden@users.noreply.github.com>
2018-02-23 17:16:50 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -3,13 +3,9 @@ ESX               = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 ESX.RegisterUsableItem('drill', function(source)
-	local xPlayer = ESX.GetPlayerFromId(source)
-	local drill = xPlayer.getInventoryItem('drill')
-	
-		if drill.count > 0 then
-			xPlayer.removeInventoryItem('drill', 1)
-			TriggerClientEvent('esx_borrmaskin:startDrill', source)
-		else
-			TriggerClientEvent('esx:showNotification', source, ('Du har ingen ~r~borrmaskin'))
-		end
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local drill = xPlayer.getInventoryItem('drill')
+
+    xPlayer.removeInventoryItem('drill', 1)
+    TriggerClientEvent('esx_borrmaskin:startDrill', source)
 end)
